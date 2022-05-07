8b836c61e337767078e8f3a57c0dda244db4304d
daniel-ssj <danielp1469@gmail.com>
2022-05-07 16:38:35 -0300

    fix favoritos

--- src/controller/PrestadorController.ts
+++ src/controller/PrestadorController.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from "express";
-import { getConnection } from "typeorm";
-import { User } from "../entities/User";
-import { createAvalicao, getPrestador, getPrestadores } from "../service/PrestadorService";
+import { addFavorito, createAvalicao, getPrestador, getPrestadores } from "../service/PrestadorService";
 import { getUser } from "../service/UserService";
 
 export const getPrestadoresHandler = async (req: Request, res: Response) => {
@@ -12,22 +10,16 @@ export const getPrestadoresHandler = async (req: Request, res: Response) => {
 
 export const addFavoritoHandler = async (req: Request, res: Response) => {
     const prestadorId = parseInt(req.params.id);
-    const prestador = await getPrestador(prestadorId)
+    const prestador = await getPrestador(prestadorId);
 
-    const { userId } = req.body
-    const user = await getUser(userId)
+    if (!prestador) return res.status(404).send('Prestador not found');
+
+    const { userId } = req.body;
+    const user = await getUser(userId);
+
+    if (!user) return res.status(404).send('User not found');
     
-    await getConnection()
-        .createQueryBuilder()
-        .relation(User, 'favoritos')
-        .of(user)
-        .add(prestador);
-
-    const favoritos = await getConnection()
-        .createQueryBuilder()
-        .relation(User, 'favoritos')
-        .of(user)
-        .loadMany();
+    const favoritos = await addFavorito(user, prestador);
     
     return res.status(201).send(favoritos);
 }
@@ -38,11 +30,11 @@ export const createAvalicaoHandler = async (req: Request, res: Response) => {
 
     const prestador = await getPrestador(prestadorId);
 
-    if (!prestador) return res.status(404).send("Prestador not found");
+    if (!prestador) return res.status(404).send('Prestador not found');
 
     const user = await getUser(userId);
 
-    if (!user) return res.status(404).send("User not found");
+    if (!user) return res.status(404).send('User not found');
 
     const newAvaliacao = await createAvalicao({
         avaliacao,
--- src/service/PrestadorService.ts
+++ src/service/PrestadorService.ts
@@ -1,3 +1,5 @@
+import { User } from "../entities/User"
+import { getConnection } from "typeorm"
 import { Avaliacao, avaliacaoInput } from "../entities/Avaliacao"
 import { Prestador } from "../entities/Prestador"
 
@@ -12,6 +14,22 @@ export const getPrestador = async (prestadorId: number) => {
     const prestador = await Prestador.findOne(prestadorId)
 
     return prestador
+}
+
+export const addFavorito = async (user: User, prestador: Prestador) => {
+    await getConnection()
+        .createQueryBuilder()
+        .relation(User, 'favoritos')
+        .of(user)
+        .add(prestador);
+
+    const favoritos = await getConnection()
+        .createQueryBuilder()
+        .relation(User, 'favoritos')
+        .of(user)
+        .loadMany();
+
+    return favoritos;
 }
 
 export const createAvalicao = async (input: avaliacaoInput) => {
