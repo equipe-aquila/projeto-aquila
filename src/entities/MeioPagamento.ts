9a2956dc77032df6fd30536d7eb3a4e4d605af6d
daniel-ssj <danielp1469@gmail.com>
2022-05-07 18:04:48 -0300

    entity meios pagamento

---
+++ src/entities/MeioPagamento.ts
@@ -0,0 +1,28 @@
+import {
+	BaseEntity, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn
+} from 'typeorm';
+import { User } from './User';
+
+@Entity('meio_pagamentos')
+export class MeioPagamento extends BaseEntity {
+    @PrimaryGeneratedColumn()
+	id: number
+
+    @Column({ length: 30 })
+	meioPagamento: string
+
+    @Column({ length: 30 })
+	numeroCartao: string
+
+    @Column({ length: 30 })
+	nomeTitular: string
+
+    @Column({ length: 30 })
+	codSeguranca: string
+
+    @Column({ length: 30 })
+	validade: string
+
+    @ManyToOne(() => User, (user) => user.meiosPagamento)
+	user: User
+}
