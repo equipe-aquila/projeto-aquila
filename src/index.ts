206981808cb7ff767064ddd53edc9d732ab456f3
daniel-ssj <danielp1469@gmail.com>
2022-05-21 10:02:35 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import routes from './routes';
 import connect from './utils/connect';
 import swaggerDocs from './utils/swagger';
+import cors from 'cors'
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.listen(process.env.PORT || 9090, async () => {
 	console.log('running on port 9090');
