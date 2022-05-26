9c06005df6e9d5ee31d09879c200fdca5e2643c7
kelvynsantos1 <kelvynsantos10@outlook.com>
2022-05-26 14:47:37 -0300

    first commit

---
+++ app/src/screens/Preload/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from './styles';
+import AquilaLogo from '../../assets/Aquila-Logo.svg'
+
+export default () => {
+    return (
+        <Container>
+            <AquilaLogo width="100%" height="160">
+
+            </AquilaLogo>
+        </Container>
+    );
+}
---
+++ app/src/screens/Preload/styles.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+    background-color: #6959CD;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
