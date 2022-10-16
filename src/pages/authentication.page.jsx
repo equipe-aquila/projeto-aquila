66fe9a9f7c70f65ff074391d842dd4fe7b4dc98d
Brendo <brendolivergon@gmail.com>
2022-10-16 11:15:03 -0300

    UI Login finalizada

--- src/pages/authentication.page.jsx
+++ src/pages/authentication.page.jsx
@@ -8,7 +8,7 @@ import { AutoCenter, Space } from "antd-mobile";
 
 import styles0 from '../modules/reset.css'
 import styles from '../modules/authentication.modules.css'
-import img1 from '../components/img/img1.jpg'
+import logo from '../components/img/logo.png'
 
 const Authenticate = () => {
     useEffect(() => {
@@ -28,29 +28,35 @@ const Authenticate = () => {
     }, []);
 
     return (
-        <div id="painel">
-            <div id="garota">
-                <img src={img1} alt="Bem-vindo"/>
-            </div>
-            <div id="caixa">
-                <form method="" action="">
-                    <h1 id="tituloLogin">Login</h1>
-                    <label for="text-email">E-mail</label>
-                    <input type="text" id="text-email"/>
-                    <label for="text-password">Senha</label>
-                    <input type="password" id="text-password"/>
-                    <input type="checkbox" id="logado" value="manterLogado" name="logado"/>
-                    <label id="labelLogado" for="logado">Manter-me logado</label>
-                    <center>
-                        <input type="submit" value="Entrar" id="botao-entrar"/>
-                        <GoogleButton label="Entrar com Google" id="googleButton" onClick={signInWithGoogleRedirect}></GoogleButton>
-                    </center>
-                    <div id="cadastro">
-                        <label for="botao-cadastrar" id="labelCadastrar">Ainda não tem conta?</label>
-                        <input type="submit" value="Cadastrar-se" id="botao-cadastrar"/>
-                    </div>
-                </form>
-            </div>
+        <div>
+            <AutoCenter>
+                <Space direction="vertical">
+                    <AutoCenter>
+                        <div id="painel">
+                            <h1 id="fantasia"><img src={logo} alt="Áquila"/></h1>
+                            <div id="caixa">
+                                <form method="" action="">
+                                    <h2 id="tituloLogin">Login</h2>
+                                    <label for="text-email">E-mail</label>
+                                    <input type="text" id="text-email"/>
+                                    <label for="text-password">Senha</label>
+                                    <input type="password" id="text-password"/>
+                                    <input type="checkbox" id="logado" value="manterLogado" name="logado"/>
+                                    <label id="labelLogado" for="logado">Manter-me logado</label>
+                                    <center>
+                                        <input type="submit" value="Entrar" id="botao-entrar"/>
+                                        <GoogleButton label="Entrar com Google" id="googleButton" onClick={signInWithGoogleRedirect}></GoogleButton>
+                                    </center>
+                                    <div id="cadastro">
+                                        <label for="botao-cadastrar" id="labelCadastrar">Ainda não tem conta?</label>
+                                        <input type="submit" value="Cadastrar-se" id="botao-cadastrar"/>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </AutoCenter>
+                </Space>
+            </AutoCenter>
         </div>
     );
 }
