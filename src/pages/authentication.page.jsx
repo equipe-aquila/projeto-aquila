f5ad2293cd7a8a21a30f843382580fc008c9bd8c
Brendo <brendolivergon@gmail.com>
2022-10-15 23:31:21 -0300

    UI Login não responsivo

--- src/pages/authentication.page.jsx
+++ src/pages/authentication.page.jsx
@@ -6,6 +6,10 @@ import { auth, signInWithGoogleRedirect } from "../utils/firebase/firebase.utils
 import GoogleButton from "react-google-button";
 import { AutoCenter, Space } from "antd-mobile";
 
+import styles0 from '../modules/reset.css'
+import styles from '../modules/authentication.modules.css'
+import img1 from '../components/img/img1.jpg'
+
 const Authenticate = () => {
     useEffect(() => {
         const authenticate = async () => {
@@ -24,15 +28,29 @@ const Authenticate = () => {
     }, []);
 
     return (
-        <div>
-            <AutoCenter>
-                <Space direction="vertical">
-                    <AutoCenter>
-                        <h1>Áquila</h1>
-                    </AutoCenter>
-                    <GoogleButton label="Entrar com Google" onClick={signInWithGoogleRedirect}></GoogleButton>
-                </Space>
-            </AutoCenter>
+        <div id="painel">
+            <div id="garota">
+                <img src={img1} alt="Bem-vindo"/>
+            </div>
+            <div id="caixa">
+                <form method="" action="">
+                    <h1 id="tituloLogin">Login</h1>
+                    <label for="text-email">E-mail</label>
+                    <input type="text" id="text-email"/>
+                    <label for="text-password">Senha</label>
+                    <input type="password" id="text-password"/>
+                    <input type="checkbox" id="logado" value="manterLogado" name="logado"/>
+                    <label id="labelLogado" for="logado">Manter-me logado</label>
+                    <center>
+                        <input type="submit" value="Entrar" id="botao-entrar"/>
+                        <GoogleButton label="Entrar com Google" id="googleButton" onClick={signInWithGoogleRedirect}></GoogleButton>
+                    </center>
+                    <div id="cadastro">
+                        <label for="botao-cadastrar" id="labelCadastrar">Ainda não tem conta?</label>
+                        <input type="submit" value="Cadastrar-se" id="botao-cadastrar"/>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
